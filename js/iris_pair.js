d42a62782db4c99cd514fc94eec11b02fb6de97c
Filip Hnízdo <filip@bluejumpers.com>
2016-01-11 18:13:56 +0000

    Socket and HTTP authentication

--- js/iris_pair.js
+++ js/iris_pair.js
@@ -26,21 +26,35 @@ var iris = {};
 
 iris.url = decodeURIComponent(readCookie("Drupal.visitor.iris_server"));
 
+iris.credentials = {
+  "userid": readCookie("Drupal.visitor.iris_userid"),
+  "token": readCookie("Drupal.visitor.iris_token"),
+};
+
 var socket = io(iris.url);
 
 socket.on("connect", function () {
-
   socket.emit('pair', {
-    credentials: {
-      "userid": readCookie("Drupal.visitor.iris_userid"),
-      "token": readCookie("Drupal.visitor.iris_token"),
-    }
+    credentials: iris.credentials
   });
 });
 
 socket.on("pair", function (result) {
 
   if (result) {
+
+    jQuery.ajax({
+      url: iris.url + "/auth/checkauth",
+      data: {
+        credentials: iris.credentials
+      }
+    }).done(function (data) {
+
+      // Example call saving the authPass from Iris
+      
+      iris.authPass = data;
+
+    });
 
     console.log("Paired with Iris");
 
